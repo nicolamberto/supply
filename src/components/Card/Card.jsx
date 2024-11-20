17d6907da6cf447939d2f8cef5932d1598a85059
Juan Manuel <losredondos551@gmail.com>
2024-11-20 15:59:40 -0300

    boton agregado

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,40 +2,47 @@
 import ProductBadge from "../ProductBadge/ProductBadge";
 import style from "./Card.module.css";
 import iconEcoFriend from "../../assets/isotipos/frend.png";
+import { useState } from "react";
+import { FaRegCheckCircle } from "react-icons/fa";
 
 function Card({ name, image, description, technicalDetails, addToCart, id }) {
+  const [added, setAdded] = useState(false);
 
+  const handleClick = (id) => {
+    setAdded(true);
+    addToCart(id)
+  };
   return (
-    <div className={style.card_main}>
-      <span className={style.name_product}>{name}</span>
-      <div className={style.container_image}>
-        <img className={style.product_image} src={image} alt="producto" />
-      </div>
-      <div className={style.container_detail}>
-        <div className={style.container_isotipos}>
-          {technicalDetails.capacity && (
-            <ProductBadge item={technicalDetails.capacity} />
-          )}
-          {technicalDetails.loadCapacity && (
-            <ProductBadge item={technicalDetails.loadCapacity} />
-          )}
-          {technicalDetails.thermalSpectrum && (
-            <ProductBadge item={technicalDetails.thermalSpectrum} />
-          )}
-          {technicalDetails.ecofriendly ? (
-            <div className={style.container_eco}>
-              <img src={iconEcoFriend} alt="ecofriendly" />
-            </div>
-          ) : null}
+<div className={style.card_main}>
+  <span className={style.name_product}>{name}</span>
+  <div className={style.container_image}>
+    <img className={style.product_image} src={image} alt="producto" />
+  </div>
+  <div className={style.container_detail}>
+    <div className={style.container_isotipos}>
+      {technicalDetails.capacity && (
+        <ProductBadge item={technicalDetails.capacity} />
+      )}
+      {technicalDetails.loadCapacity && (
+        <ProductBadge item={technicalDetails.loadCapacity} />
+      )}
+      {technicalDetails.thermalSpectrum && (
+        <ProductBadge item={technicalDetails.thermalSpectrum} />
+      )}
+      {technicalDetails.ecofriendly ? (
+        <div className={style.container_eco}>
+          <img src={iconEcoFriend} alt="ecofriendly" />
         </div>
-        <div className={style.container_title}>
-          <p>{description}</p>
-          <button onClick={(e) => addToCart(id)} className={style.button}>
-            Agregar al pedido
-          </button>
-        </div>
-      </div>
+      ) : null}
     </div>
+    <div className={style.container_title}>
+      <p>{description}</p>
+      <button onClick={(e) => handleClick(id)} className={`${added ? style.button_add : style.button}`}>
+        {added ? "Agregado" : "Agregar al pedido"}
+      </button>
+    </div>
+  </div>
+</div>
   );
 }
 
