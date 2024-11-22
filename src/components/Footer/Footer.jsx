6ae11cc880ad0a592d0a5ea6df02d1a44eafef98
Juan Manuel <losredondos551@gmail.com>
2024-11-22 09:37:56 -0300

    Link google maps

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,7 @@
 import style from "./Footer.module.css";
+import { SiGooglemaps } from "react-icons/si";
 import { SiFacebook, SiInstagram, SiWhatsapp } from "react-icons/si";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
@@ -18,7 +20,21 @@ function Footer() {
         <span>
           <strong>Dirección</strong>
         </span>
-        <p>Neuquén 4030, Villa Ballester, Provincia de Buenos Aires.</p>
+
+        <p>
+          {" "}
+          <Link
+            target="_blank"
+            rel="noopener noreferrer"
+            to={
+              "https://www.google.com/maps/place/Neuqu%C3%A9n+4030,+B1653+Villa+Ballester,+Provincia+de+Buenos+Aires,+Argentina/@-34.558382,-58.566868,13z/data=!4m6!3m5!1s0x95bcb9f0621e479d:0xaf9571b43b080a7a!8m2!3d-34.5583817!4d-58.5668682!16s%2Fg%2F11h8ch4czt?hl=es-ES&entry=ttu&g_ep=EgoyMDI0MTExOS4yIKXMDSoASAFQAw%3D%3D"
+            }
+            style={{ color: "white", textDecoration: "none" }}
+          >
+            <SiGooglemaps style={{ fontSize: "1.2rem" }} />
+          </Link>{" "}
+          Neuquén 4030, Villa Ballester, Provincia de Buenos Aires.
+        </p>
         <span>
           <strong>Horario</strong>
         </span>
